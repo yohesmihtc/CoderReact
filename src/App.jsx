a781e60ecd6344d18904be19b85be535b516bdf5
Yohesmiht Colmenarez <yohesmiht0298@gmail.com>
2024-11-20 02:16:21 +0000

    Preentrega final en proceso

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
 import Cart from './components/Cart/Cart'
-// import Checkout from './components/Checkout/Checkout'
+//sds import Checkout from './components/Checkout/Checkout'
 
 import { CartProvider } from './context/CartContext'
 
